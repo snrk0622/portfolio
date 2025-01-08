f546264d15b1f0c77beb6ff7214b429fb051a2b9
snrk0622 <snrk.0622@gmail.com>
2025-01-08 22:44:35 +0900

    余白調節

--- src/app/_components/_common/_main-column/_common/main-column-container.tsx
+++ src/app/_components/_common/_main-column/_common/main-column-container.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const MainColumnContainer: React.FC<Props> = ({ title, category, border= true, children }) => {
   return (
-    <div className="mb-6">
+    <div className="mb-10">
       <div className="flex justify-between items-center">
-        {title && <div className="text-5xl font-bold">{title}</div>}
-        {category && <Link href={`/categories/${category}`} className='hover:underline'>see more ...</Link>}
+        {title && <div className="text-3xl md:text-5xl font-bold mb-2">{title}</div>}
+        {category && <Link href={`/categories/${category}`} className='hover:underline text-xs'>もっと見る ＞</Link>}
       </div>
       <div className={`${border && 'border p-4'} border-light-sub dark:border-dark-sub`}>{children}</div>
     </div>
--- src/app/_components/_common/_side-column/_common/side-column-container.tsx
+++ src/app/_components/_common/_side-column/_common/side-column-container.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 const SideColumnContainer: React.FC<Props> = ({ title, border= true, children }) => {
   return (
-    <div className="mb-6">
-      {title && <div className="text-5xl font-bold md:text-lg md:font-bold mb-1">{title}</div>}
+    <div className="mb-10">
+      {title && <div className="text-3xl font-bold md:text-lg md:font-bold mb-2 md:mb-1">{title}</div>}
       <div className={`${border && 'border p-4'} border-light-sub dark:border-dark-sub`}>{children}</div>
     </div>
   );

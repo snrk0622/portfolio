c551008cff13ac598dfcb016a72b8d21c99f297c
snrk0622 <snrk.0622@gmail.com>
2025-01-08 23:50:01 +0900

    フッターにアイコン追加

--- src/app/_components/_layout/footer.tsx
+++ src/app/_components/_layout/footer.tsx
@@ -4,6 +4,7 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <div className="py-10 text-center">
+        <img src="/assets/_author/snrk_transparent.png" className="w-10 h-10 mx-auto mb-5" alt="snrk cat" />
         <Link
           href="https://github.com/snrk0622/portfolio"
           className="mx-3 font-bold hover:underline"

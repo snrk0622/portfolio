a6f6ae646331515329ab8e222b54891f745cb0b5
snrk0622 <snrk.0622@gmail.com>
2025-01-08 23:51:25 +0900

    テキストスタイル微調整

--- src/app/_components/_layout/footer.tsx
+++ src/app/_components/_layout/footer.tsx
@@ -7,9 +7,9 @@ export function Footer() {
         <img src="/assets/_author/snrk_transparent.png" className="w-10 h-10 mx-auto mb-5" alt="snrk cat" />
         <Link
           href="https://github.com/snrk0622/portfolio"
-          className="mx-3 font-bold hover:underline"
+          className="mx-3 hover:underline"
         >
-          View code on GitHub
+          View code on <span className="font-bold">GitHub</span>
         </Link>
       </div>
     </footer>
